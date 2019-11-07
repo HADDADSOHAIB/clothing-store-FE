994a979dcef914034f4b37e47c2e1026cb062b80
Sohaib HADDAD <souhaib.haddad@gmail.com>
2019-11-07 18:45:10 +0100

    add product list component initial files

--- FE/src/app/admin/component/product-list/product-list.component.ts
+++ FE/src/app/admin/component/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product-service/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
   }
 
+  
 }
