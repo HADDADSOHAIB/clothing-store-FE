51fe935ecc001fe137ce7b910d9ffca795e5c664
HADDADSOHAIB <souhaib.haddad@gmail.com>
2019-11-13 19:43:59 +0100

    remove BE

--- BE/SocialECommerce/SocialECommerce/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace SocialECommerce.Services
-{
-    public interface IAuthService
-    {
-        string GenerateJwtToken(IdentityUser user);
-    }
-}
